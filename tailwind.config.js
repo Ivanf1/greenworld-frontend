4f6b5da384862fee06a993ee0a4bd155188c3549
Ivan <capobianco.ivan@gmail.com>
2022-05-28 17:22:45 +0200

    build: tailwind css customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0d6651",
+        "primary-tint": "#CBDCD8",
+        "light-grey": "#F6F6F6",
+        "accent-blue": "#0076B1",
+        "accent-red": "#D64E78",
+        "accent-purple": "#8E0072",
+        "accent-purple-tint": "#D299C7",
+      },
+    },
   },
   plugins: [],
 };
