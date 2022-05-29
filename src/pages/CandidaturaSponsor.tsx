69f97b4cad809db855d0d68943ef7c83872d1831
Ivan <capobianco.ivan@gmail.com>
2022-05-29 21:40:26 +0200

    fix: responsive form

--- src/pages/CandidaturaSponsor.tsx
+++ src/pages/CandidaturaSponsor.tsx
@@ -1,7 +1,7 @@
 const CandidaturaSponsor = () => {
   return (
-    <div className="form-background min-h-screen py-10 bg-primary-tint">
-      <div className="card flex flex-col items-center p-6 md:p-[60px] max-w-[95%] md:max-w-2xl mx-auto rounded-lg">
+    <div className="form-background min-h-full py-10 bg-primary-tint">
+      <div className="card flex flex-col items-center p-6 md:p-[60px] max-w-[95%] md:max-w-2xl md:w-2xl mx-auto rounded-lg">
         <form className="w-full">
           <h4 className="mb-8">Candidati come sponsor</h4>
           <div className="my-3">
