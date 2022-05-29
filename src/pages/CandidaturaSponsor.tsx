ed1cc695f1195255093d4cf4e4d71c5e0ca6f281
Ivan <capobianco.ivan@gmail.com>
2022-05-29 20:08:31 +0200

    refactor: tailwindcss classes for form

--- src/pages/CandidaturaSponsor.tsx
+++ src/pages/CandidaturaSponsor.tsx
@@ -1,47 +1,47 @@
 const CandidaturaSponsor = () => {
   return (
-    <div className="form-background">
-      <div className="form-card">
-        <form className="">
+    <div className="form-background min-h-screen py-10 bg-primary-tint">
+      <div className="card flex flex-col items-center p-6 md:p-[60px] max-w-[95%] md:max-w-2xl mx-auto rounded-lg">
+        <form className="w-full">
           <h4 className="mb-8">Candidati come sponsor</h4>
-          <div className="form-field">
-            <label className="form-label" htmlFor="azienda">
+          <div className="my-3">
+            <label className="block" htmlFor="azienda">
               Nome azienda
             </label>
-            <input className="text-input" type="text" name="azienda" />
+            <input className="input" type="text" name="azienda" required />
           </div>
-          <div className="form-field">
-            <label className="form-label" htmlFor="referenze">
+          <div className="my-3">
+            <label className="block" htmlFor="referenze">
               Referenze
             </label>
-            <input className="text-input" type="text" name="referenze" />
+            <input className="input" type="text" name="referenze" required />
           </div>
-          <div className="form-field">
-            <label className="form-label" htmlFor="email">
+          <div className="my-3">
+            <label className="block" htmlFor="email">
               E-mail
             </label>
-            <input className="text-input" type="email" name="email" />
+            <input className="input" type="email" name="email" required />
           </div>
-          <div className="form-field">
-            <label className="form-label" htmlFor="telefono">
+          <div className="my-3">
+            <label className="block" htmlFor="telefono">
               Telefono
             </label>
-            <input className="text-input" type="email" name="telefono" />
+            <input className="input" type="email" name="telefono" required />
           </div>
-          <div className="form-field">
-            <label className="form-label" htmlFor="sitoweb">
+          <div className="my-3">
+            <label className="block" htmlFor="sitoweb">
               URL sito web
             </label>
-            <input className="text-input" type="email" name="sitoweb" />
+            <input className="input" type="email" name="sitoweb" required />
           </div>
-          <div className="form-field">
-            <label className="form-label" htmlFor="logo">
+          <div className="my-3">
+            <label className="block" htmlFor="logo">
               Carica il tuo logo
             </label>
-            <input className="text-input" type="file" name="logo" />
+            <input className="input" type="file" name="logo" />
           </div>
           <div className="flex justify-end mt-[20px]">
-            <button className="primary">Candidati</button>
+            <button className="primary w-full md:w-auto">Candidati</button>
           </div>
         </form>
       </div>
