662df7e22d067d1c6c0a3ec36b87f47a0c445e8e
Ivan <capobianco.ivan@gmail.com>
2022-06-09 16:36:49 +0200

    refactor: improve components separation

--- src/components/SegnalazioneCard.tsx
+++ src/components/SegnalazioneCard.tsx
@@ -1,8 +1,21 @@
 import Carousel from "./Carousel";
-import profileImg from "../assets/profile-img.png";
 import mapMarkImg from "../assets/map-mark.svg";
+import UserComment from "./UserComment";
+import profileImg from "../assets/profile-img.png";
 
 const SegnalazioneCard = () => {
+  const comment =
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque \
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi \
+        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit\
+        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione\
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,\
+        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et\
+        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum\
+        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
+        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil\
+        molestiae consequatur, vel illum qui dolorem eum fugiat.";
+
   return (
     <div className="card p-4 md:p-10 flex flex-col space-y-10 md:space-y-20 w-[95%] max-w-[95%] md:max-w-[1000px]">
       <div className="grid grid-cols-1 grid-rows-[repeat(3,auto)] gap-y-5 lg:grid-cols-2 lg:grid-rows-[repeat(2,auto)] lg:gap-10">
@@ -28,24 +41,7 @@ const SegnalazioneCard = () => {
         </div>
       </div>
 
-      <div>
-        <div className="flex items-center space-x-5">
-          <img className="profile-img" src={profileImg} alt="" />
-          <h4 className="font-semibold">Marge</h4>
-        </div>
-        <p className="mt-4 md:pl-[100px]">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-          architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-          voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-          amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-          labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-          consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-          nihil molestiae consequatur, vel illum qui dolorem eum fugiat.
-        </p>
-      </div>
+      <UserComment name="Marge" profileImg={profileImg} comment={comment} />
     </div>
   );
 };
--- src/pages/Evento.tsx
+++ src/pages/Evento.tsx
@@ -4,12 +4,25 @@ import calendarImg from "../assets/calendar.svg";
 import timeImg from "../assets/time.svg";
 import sponsorLogo from "../assets/pizzaciro.png";
 import profileImg from "../assets/profilo.png";
+import commentProfileImg from "../assets/profile-img.png";
 import UserComment from "../components/UserComment";
 
 const Evento = () => {
   const partecipanti = 45;
   const maxPartecipanti = 150;
   const numeroCommenti = 10;
+
+  const comment =
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque \
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi \
+        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit\
+        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione\
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,\
+        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et\
+        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum\
+        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
+        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil\
+        molestiae consequatur, vel illum qui dolorem eum fugiat.";
 
   return (
     <div className="w-full h-full bg-light-grey py-10">
@@ -102,7 +115,9 @@ const Evento = () => {
 
         <div className="flex flex-col space-y-10 mt-20 max-w-[1000px]">
           {[...Array(numeroCommenti)].map((_, i) => {
-            return <UserComment key={i} />;
+            return (
+              <UserComment key={i} name="Marge" profileImg={commentProfileImg} comment={comment} />
+            );
           })}
         </div>
       </div>
