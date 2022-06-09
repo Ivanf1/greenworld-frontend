fdaaa7a217f240c2a3486bef479e30c571f6ba5c
Ivan <capobianco.ivan@gmail.com>
2022-06-09 13:11:04 +0200

    fix: segnalazione card responsiveness

--- src/components/SegnalazioneCard.tsx
+++ src/components/SegnalazioneCard.tsx
@@ -4,29 +4,31 @@ import mapMarkImg from "../assets/map-mark.svg";
 
 const SegnalazioneCard = () => {
   return (
-    <div className="card p-4 md:p-10 flex flex-col space-y-8 md:space-y-5 w-[95%] max-w-[95%] md:max-w-[1000px]">
-      <div className="grid grid-cols-1 gap-y-5 md:grid-cols-2 md:gap-10">
-        <div className="row-start-2 md:row-start-auto">
+    <div className="card p-4 md:p-10 flex flex-col space-y-10 md:space-y-20 w-[95%] max-w-[95%] md:max-w-[1000px]">
+      <div className="grid grid-cols-1 grid-rows-[repeat(3,auto)] gap-y-5 lg:grid-cols-2 lg:grid-rows-[repeat(2,auto)] lg:gap-10">
+        <div className="row-start-2 lg:row-start-1 lg:row-span-2">
           <Carousel />
         </div>
+
         <div className="flex flex-1 flex-col space-y-3">
           <h3 className="font-bold text-xl">Ma che ne so</h3>
-          <p>
-            lorem ipsum dolor sir lorem ipsum dolor sit lorem ipsum dolor sir lorem dolor sit bla
-            lorem ipsum dolor sir lorem ipsum dolor
-          </p>
           <div className="flex items-start space-x-2">
             <img src={mapMarkImg} alt="" />
             <p className="">Via di qua</p>
           </div>
-          <div className="flex flex-col md:flex-row md:flex-1 md:items-end space-y-2 my-8 md:mt-0 md:space-x-5 md:mb-5">
-            <button className="primary">Crea evento</button>
-            <button className="delete">Rimuovi</button>
-          </div>
+          <p>
+            lorem ipsum dolor sir lorem ipsum dolor sit lorem ipsum dolor sir lorem dolor sit bla
+            lorem ipsum dolor sir lorem ipsum dolor
+          </p>
+        </div>
+
+        <div className="flex flex-col md:flex-row md:items-end space-y-2 md:mt-0 md:space-x-5 md:mb-5">
+          <button className="primary">Crea evento</button>
+          <button className="delete">Rimuovi</button>
         </div>
       </div>
-      <div className="separator w-full my-5"></div>
-      <div className="">
+
+      <div>
         <div className="flex items-center space-x-5">
           <img className="profile-img" src={profileImg} alt="" />
           <h4 className="font-semibold">Marge</h4>
