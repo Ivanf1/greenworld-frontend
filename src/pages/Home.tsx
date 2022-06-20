028817c6299ab23052b71b3487097561105e629b
Ivan <capobianco.ivan@gmail.com>
2022-06-20 13:47:38 +0200

    feat: sponsor section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,16 @@ import gridImg1 from "../assets/home-grid-1.jpg";
 import gridImg2 from "../assets/home-grid-2.jpg";
 import gridImg3 from "../assets/home-grid-3.jpg";
 import downArrow from "../assets/down.svg";
+
+import sponsor1 from "../assets/logos/alpine-13.svg";
+import sponsor2 from "../assets/logos/aprilia-26418.svg";
+import sponsor3 from "../assets/logos/netflix-4.svg";
+import sponsor4 from "../assets/logos/chevrolet.svg";
+import sponsor5 from "../assets/logos/hilti-1.svg";
+import sponsor6 from "../assets/logos/honda-7.svg";
+import sponsor7 from "../assets/logos/shopify-2.svg";
+import sponsor8 from "../assets/logos/hulu-2.svg";
+
 import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 
@@ -76,7 +86,7 @@ const Home = () => {
       </section>
 
       <section className="bg-light-grey p-5 lg:py-20">
-        <div className="grid grid-cols-1 grid-rows-[repeat(6,auto)]  md:grid-cols-2 md:grid-rows-3 md:-y-10 lg:-y-20 lg:max-w-[78.125rem] mx-auto w-full h-full">
+        <div className="grid grid-cols-1 grid-rows-[repeat(6,auto)] md:grid-cols-2 md:grid-rows-3 md:-y-10 lg:-y-20 lg:max-w-[78.125rem] mx-auto w-full h-full">
           <div className="md:home-card-shadow top-border md:border-none">
             <img src={gridImg1} alt="" className="w-full md:h-full  md:object-cover" />
           </div>
@@ -130,19 +140,44 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="w-full bg-primary-tint hidden lg:block">
-        <div className="grid grid-cols-[30%_auto] py-60 max-w-[78.125rem] mx-auto">
-          <div className="flex flex-col items-start space-y-6 px-5 xl:px-0">
-            <h2 className="text-xl font-semibold">La tua azienda per il cambiamento climatico</h2>
+      <section className="w-full bg-primary-tint">
+        <div className="grid grid-cols-1 grid-rows-[repeat(3,auto)] lg:grid-cols-[28%_auto] lg:grid-rows-[repeat(2,auto)] max-w-[78.125rem] mx-auto lg:gap-x-16 xl:gap-x-48 py-10 lg:py-0 gap-y-5 lg:gap-y-0">
+          <div className="flex flex-col items-center lg:items-start space-y-6 px-5 xl:px-0 lg:pt-60">
+            <h2 className="text-xl font-semibold text-center lg:text-left">
+              La tua azienda per il cambiamento climatico
+            </h2>
+          </div>
+          <div className="flex flex-col items-center lg:items-start col-start-1 col-end-1 lg:row-start-2 lg:row-end-2 lg:pb-60 lg:mt-4 px-5 xl:px-0">
             <p>
-              234 aziende si sono già unite alla nostra causa. Diventa anche tu parte del
-              cambiamento.
+              <span className="font-semibold">234</span> aziende si sono già unite alla nostra
+              causa.
             </p>
-            <button className="primary" onClick={() => navigate("candidatura-sponsor")}>
+            <p>Diventa anche tu parte del cambiamento.</p>
+            <button className="primary mt-5" onClick={() => navigate("candidatura-sponsor")}>
               Diventa Sponsor
             </button>
           </div>
-          <div></div>
+          <div className="flex flex-col justify-around col-start-1 col-end-1 row-start-2 row-end-2 lg:col-start-2 lg:col-end-2 lg:row-start-1 lg:row-span-2 px-5 xl:px-0">
+            <div className="flex justify-around w-full py-10">
+              <img src={sponsor1} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor2} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor3} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor4} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+            </div>
+            <div className="flex justify-between w-full py-5">
+              <img src={sponsor5} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor6} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor7} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor8} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor1} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+            </div>
+            <div className="flex justify-around w-full py-10">
+              <img src={sponsor2} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor4} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor6} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+              <img src={sponsor5} alt="" className="w-[50px] md:w-[70px] lg:w-[80px] h-auto" />
+            </div>
+          </div>
         </div>
       </section>
 
